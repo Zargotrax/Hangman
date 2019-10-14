2b9e6de2a4f6aca865a04857ff4c55a597906f43
Zargotrax <mamorneau@hotmail.ca>
2019-10-14 12:43:51 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,98 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <div>
+        {{ lives}}
+      </div>
+      <div class="button" v-for="item in this.alphabet">
+          <button v-on:click="click(item.letter), checkStatus()"> {{ item.letter }} </button>
+      </div>
+    </div>   
+    Selected Letters : 
+    <div class="letter" v-for="item in selectedLetters">
+      {{ item }}
+    </div>
+    <div>
+      Word to guess : 
+      <div class="letter" v-for="letter in randomWord.toUpperCase()">
+        <h1 v-if="selectedLetters.includes(letter)">{{ letter }}</h1>
+        <h1 v-else>_</h1>   
+      </div>
+    </div>    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld,
+  data: function () { 
+    return {
+      lives: 5,
+      total: 0,
+      randomWord : '',
+      wordList : [
+        "locomotive",
+        "television",
+        "manequin",
+        "livre",
+        "cercueil",
+        "cyclope",
+      ],
+      selectedLetters: [],
+      alphabet: [
+        { letter: 'A' },
+        { letter: 'B' },
+        { letter: 'C' },
+        { letter: 'D' },
+        { letter: 'E' },
+        { letter: 'F' },
+        { letter: 'G' },
+        { letter: 'H' },
+        { letter: 'I' },
+        { letter: 'J' },
+        { letter: 'K' },
+        { letter: 'L' },
+        { letter: 'M' },
+        { letter: 'N' },
+        { letter: 'O' },
+        { letter: 'P' },
+        { letter: 'Q' },
+        { letter: 'R' },
+        { letter: 'S' },
+        { letter: 'T' },
+        { letter: 'U' },
+        { letter: 'V' },
+        { letter: 'X' },
+        { letter: 'Y' },
+        { letter: 'Z' },
+      ],
+    };
   },
-};
+  methods: {
+    click: function (char) {
+      if((!(this.randomWord.toUpperCase().includes(char))) && !this.selectedLetters.includes(char)){
+        this.lives -= 1;
+        this.selectedLetters.push(char)
+      } else if(this.randomWord.toUpperCase().includes(char) && !this.selectedLetters.includes(char)){
+        this.selectedLetters.push(char)
+        this.total += 1;
+      }
+      
+    },
+    checkStatus: function(){
+      if(this.lives <= 0){
+        alert("You lost! Reload to restart.")
+      } else if(this.total == this.randomWord.length){
+        alert("You win!")
+      }
+      
+    }
+  },
+
+  created (){
+    this.randomWord =  this.wordList[Math.floor(Math.random() * this.wordList.length)]
+  },
+}
 </script>
 
 <style lang="scss">
@@ -24,5 +103,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.button {
+  height: 2em;
+  width: 2em;
+  padding: 0.2em;
+  display: inline-block;
+}
+.letter {
+  height: 2em;
+  width: 2em;
+  padding: 0.2em;
+  display: inline-block;
 }
 </style>
